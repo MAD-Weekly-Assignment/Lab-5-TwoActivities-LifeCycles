f862046c3fd11d0fc9e1ec51fefed4c62703f440
AshishManandhar9 <aashishmanandhar99@gmail.com>
2021-03-17 20:52:58 +0545

    Displaying items in MainActivity

--- ShoppingList/app/src/main/java/com/example/shopping_list/SecondActivity.java
+++ ShoppingList/app/src/main/java/com/example/shopping_list/SecondActivity.java
@@ -5,56 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
-    public static final String EXTRA_ITEMS = "com.example.android.twoactivities.extra.ITEMS";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-
     }
 
-    public void selectItem(View view) {
-        Intent return_items_intent = new Intent();
 
-        switch (view.getId()){
-            case R.id.button_Chocolate:
-                return_items_intent.putExtra(EXTRA_ITEMS, "Chocolate"); setResult(RESULT_OK, return_items_intent); finish();
-                break;
-            case R.id.button_oil:
-                return_items_intent.putExtra(EXTRA_ITEMS, "Oil"); setResult(RESULT_OK, return_items_intent); finish();
-                break;
-            case R.id.button_milk:
-                return_items_intent.putExtra(EXTRA_ITEMS, "Milk"); setResult(RESULT_OK, return_items_intent); finish();
-                break;
-            case R.id.button_eggs:
-                return_items_intent.putExtra(EXTRA_ITEMS, "Eggs"); setResult(RESULT_OK, return_items_intent); finish();
-                break;
-            case R.id.button_cheese:
-                return_items_intent.putExtra(EXTRA_ITEMS, "Cheese"); setResult(RESULT_OK, return_items_intent); finish();
-                break;
-            case R.id.button_rice:
-                return_items_intent.putExtra(EXTRA_ITEMS, "Rice"); setResult(RESULT_OK, return_items_intent); finish();
-                break;
-            case R.id.button_wheat:
-                return_items_intent.putExtra(EXTRA_ITEMS, "Wheat Flour"); setResult(RESULT_OK, return_items_intent); finish();
-                break;
-            case R.id.button_bread:
-                return_items_intent.putExtra(EXTRA_ITEMS, "Bread"); setResult(RESULT_OK, return_items_intent); finish();
-                break;
-            case R.id.button_sugar:
-                return_items_intent.putExtra(EXTRA_ITEMS, "Sugar"); setResult(RESULT_OK, return_items_intent); finish();
-                break;
-            case R.id.button_salt:
-                return_items_intent.putExtra(EXTRA_ITEMS, "Salt"); setResult(RESULT_OK, return_items_intent); finish();
-                break;
-            default:
-                Toast.makeText(SecondActivity.this, "Error Detected", Toast.LENGTH_LONG).show();
-                break;
-        }
+    public void addItemToShoppingList(View view) {
+        Button shoppingItemBtn = findViewById(view.getId());
 
+        String shoppingItem = shoppingItemBtn.getText().toString();
+
+        Intent replyIntent = new Intent(this, MainActivity.class);
+        replyIntent.putExtra("shoppingItem", shoppingItem);
+        setResult(RESULT_OK, replyIntent);
+        finish();
     }
 }
