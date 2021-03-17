c49fa43128f238588f1d57346667fde6f4c8113b
AshishManandhar9 <aashishmanandhar99@gmail.com>
2021-03-17 19:50:13 +0545

    Sending Data from MainActivity to SecondActivity

--- ShoppingList/app/src/main/java/com/example/shopping_list/MainActivity.java
+++ ShoppingList/app/src/main/java/com/example/shopping_list/MainActivity.java
@@ -65,7 +65,25 @@ public class MainActivity extends AppCompatActivity {
 
         startActivityForResult(intent, ITEM_REQUEST);
     }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == ITEM_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String itemString = data.getStringExtra(SecondActivity.EXTRA_ITEMS);
+                itemsList.add(itemString);
+                int i;
+                for (i = 0; i < itemsList.size(); i++) {
+                    if (itemsList.size() > 10) {
+                        Toast.makeText(MainActivity.this, "Cannot add more items to the list", Toast.LENGTH_LONG).show();
+                        break;
+                    }
+                    item[i].setVisibility(View.VISIBLE);
+                    item[i].setText(itemsList.get(i));
+                }
 
-
-
+            }
+        }
+    }
 }
